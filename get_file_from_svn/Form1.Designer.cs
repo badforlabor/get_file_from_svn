0a8438558cbecc57f9327b812a21b8ab63e8be61
badlabor <badlabor@work.com>
2018-05-21 09:19:09 +0800

    过滤svn信息

---
+++ get_file_from_svn/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace get_file_from_svn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSvn = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSvn
+            // 
+            this.txtSvn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtSvn.Location = new System.Drawing.Point(43, 88);
+            this.txtSvn.Multiline = true;
+            this.txtSvn.Name = "txtSvn";
+            this.txtSvn.Size = new System.Drawing.Size(308, 350);
+            this.txtSvn.TabIndex = 0;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(491, 88);
+            this.txtFile.Multiline = true;
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(306, 350);
+            this.txtFile.TabIndex = 1;
+            this.txtFile.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFile_KeyDown);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(358, 3);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "过滤";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 534);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.txtSvn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSvn;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ get_file_from_svn/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace get_file_from_svn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            var svn = txtSvn.Text;
+            var lines = svn.Split('\n');
+            var files = new List<string>();
+            foreach(var line in lines)
+            {
+                string linetext = line.Trim();
+
+                {
+                    string tag = "Modified : ";
+                    if (linetext.StartsWith(tag))
+                    {
+                        string txt = linetext.Substring(tag.Length);
+                        txt = txt.Trim() + "\n";
+                        if (!files.Contains(txt))
+                        {
+                            files.Add(txt);
+                        }
+                    }
+                }
+
+                {
+                    string tag = "Added : ";
+                    if (linetext.StartsWith(tag))
+                    {
+                        string txt = linetext.Substring(tag.Length);
+                        txt = txt.Trim() + "\n";
+                        if (!files.Contains(txt))
+                        {
+                            files.Add(txt);
+                        }
+                    }
+                }
+            }
+
+            txtFile.Text = string.Concat(files);
+        }
+
+        private void txtFile_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if (e.Control)
+                    {
+                        txtFile.SelectAll();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
